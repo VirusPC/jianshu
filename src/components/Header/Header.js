556aad9b908c9d5eec3f41e074b285eb4a92118f
viruspc <git@viruspc.com>
2021-09-13 23:47:22 +0800

    refactor(Header): refactor to stateless component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,60 +1,53 @@
-import { Component } from 'react';
 import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button, SearchWrapper } from "./style";
 import { CSSTransition } from 'react-transition-group';
 import { connect } from 'react-redux'
-import { getSearchFocusAction, getSearchBlurAction} from '../../store/actionCreators'
+import { getSearchFocusAction, getSearchBlurAction } from '../../store/actionCreators'
 
-class Header extends Component {
+const Header = (props) => {
+  const { focused } = props;
+  const { handleInputFocus, handleInputBlur } = props;
+  return (
+    <HeaderWrapper>
+      <Logo></Logo>
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载App</NavItem>
+        <NavItem className='right'>登录</NavItem>
+        <NavItem className='right'>
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <SearchWrapper>
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const {focused} = this.props;
-    const {handleInputFocus, handleInputBlur} = this.props;
-    return (
-      <HeaderWrapper>
-        <Logo></Logo>
-        <Nav>
-          <NavItem className='left active'>首页</NavItem>
-          <NavItem className='left'>下载App</NavItem>
-          <NavItem className='right'>登录</NavItem>
-          <NavItem className='right'>
-            <i className="iconfont">&#xe636;</i>
-          </NavItem> 
-          <SearchWrapper>
-
-            <CSSTransition
-              in={focused}
-              timeout={5000}
-              classNames="slide">
-              <NavSearch
-                className={focused ? "focused" : ""}
-                onFocus={handleInputFocus}
-                onBlur={handleInputBlur}
-              ></NavSearch>
-            </CSSTransition>
-            <i
-              className={focused ? "focused iconfont" : "iconfont"}
-            >
-              &#xe623;
-            </i>
-          </SearchWrapper>
-        </Nav>
-        <Addition>
-          <Button className='writting'>
-            <i className="iconfont">&#xe615;</i>写文章
-          </Button>
-          <Button className='reg'>
-            注册
-          </Button>
-        </Addition>
-      </HeaderWrapper>
-    )
-  }
-
+          <CSSTransition
+            in={focused}
+            timeout={5000}
+            classNames="slide">
+            <NavSearch
+              className={focused ? "focused" : ""}
+              onFocus={handleInputFocus}
+              onBlur={handleInputBlur}
+            ></NavSearch>
+          </CSSTransition>
+          <i
+            className={focused ? "focused iconfont" : "iconfont"}
+          >
+            &#xe623;
+          </i>
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Button className='writting'>
+          <i className="iconfont">&#xe615;</i>写文章
+        </Button>
+        <Button className='reg'>
+          注册
+        </Button>
+      </Addition>
+    </HeaderWrapper>
+  )
 }
+
+
 
 const mapStateToProps = (state) => {
   return {
@@ -64,10 +57,10 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleInputFocus(){
+    handleInputFocus() {
       dispatch(getSearchFocusAction());
     },
-    handleInputBlur(){
+    handleInputBlur() {
       dispatch(getSearchBlurAction());
     }
   }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const preloadedState = {
   focused: false
 }
@@ -8,7 +9,8 @@ const preloadedState = {
 const store = createStore(
   reducer,
   preloadedState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers()
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
